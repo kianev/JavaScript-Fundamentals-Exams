cd3f5a3b458dfafb6ec4ea24adccd9b8d645e067
Kaloian Ianev <kianev@gmail.com>
2017-04-24 13:01:41 +0300

    solutions

---
+++ 31.01.2016/1. Exam Results.js
@@ -0,0 +1,38 @@
+function examResult(input) {
+    let courseNeeded = input.pop().trim();
+    let average = 0;
+    let counter = 0;
+
+    for (let i = 0; i < input.length; i++) {
+        let [name, course, examPoints, bonusPoints] = input[i].split(" ");
+        examPoints = Number(examPoints);
+        bonusPoints = Number(bonusPoints);
+        if(examPoints < 100){
+            console.log(`${name} failed at "${course}"`);
+        }else{
+            let coursePoints = (examPoints / 5) + bonusPoints;
+            let grade = (coursePoints / 80) * 4 + 2;
+            if(grade > 6){
+                grade = 6.00;
+            }
+            console.log(`${name}: Exam - "${course}"; Points - ${coursePoints}; Grade - ${grade.toFixed(2)}`)
+        }
+
+        if(courseNeeded == course){
+            average += examPoints;
+            counter++;
+        }
+    }
+    let averagePoints = average/counter;
+   console.log('"' + courseNeeded + '"' + ' ' + 'average points ->' + ' '+ averagePoints);
+}
+examResult([
+    'Pesho C#-Advanced 100 3',
+    'Gosho Java-Basics 157 3',
+    'Tosho HTML&CSS 317 12',
+    'Minka C#-Advanced 57 15',
+    'Stanka C#-Advanced 157 15',
+    'Kircho C#-Advanced 300 0',
+    'Niki C#-Advanced 400 10',
+    'C#-Advanced'
+]);
